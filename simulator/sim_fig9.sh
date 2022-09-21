95c9e8408e293ff68240647e2cef2ee5156a12f5
Vadim Farutin <vfarut@gmail.com>
2022-09-21 18:40:58 +0200

    handling mkdir for existing directory

--- simulator/sim_fig9.sh
+++ simulator/sim_fig9.sh
@@ -9,19 +9,19 @@ jobs=("trace1" "trace1_pr" "trace2" "trace2_pr" "trace3" "trace3_pr" "trace4" "t
 setups=("n8g8")
 multi_resource=4
 
-mkdir results
+mkdir -p results
 echo "running..."
 for setup in ${setups[@]};do
     cluster_spec="cluster_spec/${setup}.csv"
     for job in ${jobs[@]};do
         job_file="trace-data/${job}.csv"
         log_folder="results/${setup}j${job}"
-        mkdir ${log_folder}
+        mkdir -p ${log_folder}
         # echo ${job}
         for p in ${placement[@]};do
             for s in ${schedule[@]};do
                 log_name="${log_folder}/${s}-${p}"
-                mkdir $log_name
+                mkdir -p $log_name
                 cmd="python3 run_sim.py --cluster_spec=${cluster_spec} --print --scheme=${p} --trace_file=${job_file} --schedule=${s} --log_path=${log_name} --multi_resource ${multi_resource} >tmp.out"
                 # echo ${cmd} 
                 python run_sim.py --cluster_spec=${cluster_spec} --print --scheme=${p} --trace_file=${job_file} --schedule=${s} --log_path=${log_name} --multi_resource ${multi_resource} >${log_name}/tmp.out &
