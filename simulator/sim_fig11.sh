8f49b5f9b6b304dadd5df420427dac24ac05fd42
Vadim Farutin <vfarut@gmail.com>
2022-09-23 20:53:06 +0200

    handling mkdir for existing directory

--- simulator/sim_fig11.sh
+++ simulator/sim_fig11.sh
@@ -11,19 +11,19 @@ jobs=("trace1" "trace2" "trace3" "trace4")
 setups=("n8g8")
 multi_resource=4
 
-mkdir results
+mkdir -p results
 echo "running..."
 for setup in ${setups[@]};do
     cluster_spec="cluster_spec/${setup}.csv"
     for job in ${jobs[@]};do
         job_file="trace-data/${job}.csv"
         log_folder="results/${setup}j${job}"
-        mkdir ${log_folder}
+        mkdir -p ${log_folder}
         # echo ${job}
         for p in ${placement[@]};do
             for s in ${schedule[@]};do
                 log_name="${log_folder}/${s}-${p}"
-                mkdir $log_name
+                mkdir -p $log_name
                 cmd="python3 run_sim.py --cluster_spec=${cluster_spec} --print --scheme=${p} --trace_file=${job_file} --schedule=${s} --log_path=${log_name} --multi_resource ${multi_resource} >tmp.out"
                 # echo ${cmd} 
                 python run_sim.py --cluster_spec=${cluster_spec} --print --scheme=${p} --trace_file=${job_file} --schedule=${s} --log_path=${log_name} --multi_resource ${multi_resource} >${log_name}/tmp.out &
--- simulator/sim_fig12.sh
+++ simulator/sim_fig12.sh
@@ -10,21 +10,21 @@ setups=("n8g8")
 multi_resource=4
 packing_num=(2 3 4)
 
-mkdir results
+mkdir -p results
 echo "running..."
 for setup in ${setups[@]};do
     cluster_spec="cluster_spec/${setup}.csv"
     for job in ${jobs[@]};do
         job_file="trace-data/${job}.csv"
         log_folder="results/${setup}j${job}"
-        mkdir ${log_folder}
+        mkdir -p ${log_folder}
         # echo ${job}
         for p in ${placement[@]};do
             for s in ${schedule[@]};do
                 if [ "$s" == "multi-resource-blossom-same-gpu-unaware" ]; then
                     for pn in ${packing_num[@]};do
                         log_name="${log_folder}/${s}-${p}-${pn}"
-                        mkdir $log_name
+                        mkdir -p $log_name
                         cmd="python3 run_sim.py --cluster_spec=${cluster_spec} --print --scheme=${p} --trace_file=${job_file} --schedule=${s} --log_path=${log_name} --multi_resource ${multi_resource} --packing_num ${pn} >tmp.out"
                         # echo ${cmd} 
                         python run_sim.py --cluster_spec=${cluster_spec} --print --scheme=${p} --trace_file=${job_file} --schedule=${s} --log_path=${log_name} --multi_resource ${multi_resource} --packing_num ${pn} >${log_name}/tmp.out &
